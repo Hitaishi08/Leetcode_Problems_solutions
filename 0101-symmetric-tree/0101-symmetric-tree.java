a69f848abac9ac9db64bc8fa981fd968b4289585
Hitaishi <125145810+Hitaishi08@users.noreply.github.com>
2025-04-02 12:17:59 +0530

    Time: 0 ms (100%), Space: 41.9 MB (53.07%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -14,27 +14,38 @@
  * }
  */
 class Solution {
+    public boolean check(TreeNode leftnode,TreeNode rightnode){
+        if(leftnode == null && rightnode == null)return true;
+        if((leftnode != null && rightnode == null) || (leftnode == null && rightnode != null))return false;
+
+        if(leftnode.val != rightnode.val)return false;
+
+        return check(leftnode.left,rightnode.right) && check(leftnode.right,rightnode.left);
+    }
+
     public boolean isSymmetric(TreeNode root) {
-        Queue<TreeNode> queue = new LinkedList<>();
-        if(root == null)return true;
+        // Queue<TreeNode> queue = new LinkedList<>();
+        // if(root == null)return true;
 
-        queue.offer(root.left);
-        queue.offer(root.right);
+        // queue.offer(root.left);
+        // queue.offer(root.right);
 
-        while(!queue.isEmpty()){
-            TreeNode left = queue.poll();
-            TreeNode right = queue.poll();
+        // while(!queue.isEmpty()){
+        //     TreeNode left = queue.poll();
+        //     TreeNode right = queue.poll();
 
-            if(left == null && right == null)continue;
-            if((left != null && right == null) || (left == null && right != null))return false;
-            if(left.val != right.val)return false;
+        //     if(left == null && right == null)continue;
+        //     if((left != null && right == null) || (left == null && right != null))return false;
+        //     if(left.val != right.val)return false;
 
-            queue.offer(left.left);
-            queue.offer(right.right);
-            queue.offer(left.right);
-            queue.offer(right.left);
-        }
+        //     queue.offer(left.left);
+        //     queue.offer(right.right);
+        //     queue.offer(left.right);
+        //     queue.offer(right.left);
+        // }
 
-        return true;
+        // return true;
+
+        return check(root.left,root.right);
     }
 }
