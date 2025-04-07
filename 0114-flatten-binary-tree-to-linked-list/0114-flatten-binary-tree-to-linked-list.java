dc935e635b14e690005e9789cf7c9cff371e6443
Hitaishi <125145810+Hitaishi08@users.noreply.github.com>
2025-04-07 20:16:07 +0530

    Time: 0 ms (100%), Space: 42 MB (90.79%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
@@ -14,22 +14,22 @@
  * }
  */
 class Solution {
-    public void addelement(Queue<TreeNode> q,TreeNode root){
-        if(root == null)return;
-
-        q.offer(root);
-        addelement(q,root.left);
-        addelement(q,root.right);
-    }
     public void flatten(TreeNode root) {
         if(root == null)return;
-        Queue<TreeNode> queue = new LinkedList<>();
-        addelement(queue,root);
-        queue.poll();
+
         TreeNode curr = root;
-        while(!queue.isEmpty()){
-            curr.right = queue.poll();
-            curr.left = null;
+
+        while(curr != null){
+            if(curr.left != null){
+                TreeNode temp = curr.left;
+
+                while(temp.right != null){
+                    temp = temp.right;
+                }
+                temp.right = curr.right;
+                curr.right = curr.left;
+                curr.left = null;
+            }
             curr = curr.right;
         }
     }
