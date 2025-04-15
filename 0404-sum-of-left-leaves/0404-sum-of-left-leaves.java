91d9fc91b8b70dd4168ef89adee515b60eed553f
Hitaishi <125145810+Hitaishi08@users.noreply.github.com>
2025-04-15 23:48:23 +0530

    Time: 0 ms (100%), Space: 41.3 MB (91.26%) - LeetHub

---
+++ 0404-sum-of-left-leaves/0404-sum-of-left-leaves.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public void helper(TreeNode root,int[] sum){
+        if(root == null)return;
+
+        if(root.left != null && (root.left.left == null && root.left.right == null))sum[0]+=root.left.val;
+
+        helper(root.left,sum);
+        helper(root.right,sum);
+    }
+    public int sumOfLeftLeaves(TreeNode root) {
+        int[] sum = new int[1];
+        helper(root,sum);
+        return sum[0];
+
+    }
+}
