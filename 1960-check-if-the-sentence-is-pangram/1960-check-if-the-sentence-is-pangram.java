9e529d29bba9b565743c5ad03bebb2571a8e5e84
Hitaishi <125145810+Hitaishi08@users.noreply.github.com>
2025-05-25 13:22:32 +0530

    Time: 0 ms (100%), Space: 41.8 MB (30.88%) - LeetHub

--- 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.java
+++ 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.java
@@ -9,18 +9,19 @@ class Solution {
         return true;
     }
     public boolean checkIfPangram(String sentence) {
-        if(sentence.length() < 26)return false;
-        
-        int[] freq = new int[26];
+        return SecondWay(sentence);
+        // if(sentence.length() < 26)return false;
 
-        for(int i = 0;i<sentence.length();i++){
-            freq[sentence.charAt(i) - 'a']++;
-        }
+        // int[] freq = new int[26];
 
-        for(int i = 0;i<26;i++){
-            if(freq[i] == 0)return false;
-        }
+        // for(int i = 0;i<sentence.length();i++){
+        //     freq[sentence.charAt(i) - 'a']++;
+        // }
 
-        return true;
+        // for(int i = 0;i<26;i++){
+        //     if(freq[i] == 0)return false;
+        // }
+
+        // return true;
     }
 }
