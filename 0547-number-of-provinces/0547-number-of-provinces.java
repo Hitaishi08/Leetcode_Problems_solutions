1105f38e74203b6efc3ac29cbeca41b13befd56d
Hitaishi <125145810+Hitaishi08@users.noreply.github.com>
2025-06-22 16:50:58 +0530

    Time: 1 ms (88.35%), Space: 48 MB (37.45%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,45 +1,32 @@
 class Solution {
-    public int findCircleNum(int[][] arr) {
-        HashMap<Integer,List<Integer>> map = new HashMap<>();
-        int n = arr.length;
-        int m = arr[0].length;
+    public void bfs(int node,int[][] isConnected,int[]visit){
+        int n = isConnected.length;
+        visit[node] = 1;
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(node);
 
-        for(int i = 0;i<n;i++){
-            for(int j = 0;j<m;j++){
-                if(arr[i][j] == 1){
-                    if(map.containsKey(i)){
-                        map.get(i).add(j);
-                    }else{
-                        List<Integer> list = new ArrayList<>();
-                        list.add(j);
-                        map.put(i,list);
-                    }
+        while(!q.isEmpty()){
+            int v = q.poll();
+
+            for(int i = 0;i<n;i++){
+                if(isConnected[v][i] == 1 && isConnected[v][i]==1 && visit[i] != 1){
+                    visit[i] = 1;
+                    q.offer(i);
                 }
             }
         }
-
-        int[] visit = new int[n];
-
-        int ans=0;
+    }
+    public int findCircleNum(int[][] isConnected) {
+        int n = isConnected.length;
+        int[] visit = new int[isConnected.length];
+        int count = 0;
         for(int i = 0;i<n;i++){
             if(visit[i] != 1){
-                ans++;
-                Queue<Integer> queue = new LinkedList<>();
-                queue.offer(i);
-                visit[i] = 1;
-                while(!queue.isEmpty()){
-                    int node = queue.poll();
-                    List<Integer> list = new ArrayList<>(map.get(node));
-
-                    for(int j = 0;j<list.size();j++){
-                        if(visit[list.get(j)]!= 1){
-                            queue.offer(list.get(j));
-                           visit[list.get(j)] = 1;
-                        }
-                    }
-                }
+                bfs(i,isConnected,visit);
+                count++;
             }
         }
-        return ans;
+
+        return count;
     }
 }
