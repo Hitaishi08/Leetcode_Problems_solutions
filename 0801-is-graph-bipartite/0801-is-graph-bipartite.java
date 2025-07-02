7d9c10066eff256b460c64ce1941b1ef7a18bee9
Hitaishi <125145810+Hitaishi08@users.noreply.github.com>
2025-07-03 00:05:34 +0530

    Time: 1 ms (62.68%), Space: 45.1 MB (66.08%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.java
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.java
@@ -1,51 +1,34 @@
 class Solution {
-    class Pair<U,V>{
-        U first;
-        V second;
+    public boolean BFS(int node,int[]visit,int[][] graph,char[] color){
+        Queue<Integer> q = new LinkedList<>();
 
-        Pair(U first,V second){
-            this.first = first;
-            this.second = second;
-        }
-    }
-    public boolean BFS(int node,int[][]graph,int[] visit,char[] color){
         visit[node] = 1;
-
-        Queue<Pair<Integer,Integer>> q = new LinkedList<>();
-        q.offer(new Pair<>(node,-1));
+        q.offer(node);
         color[node] = 'Y';
 
         while(!q.isEmpty()){
-            Pair<Integer,Integer> data = q.poll();
+            int v = q.poll(); 
 
-            int v = data.first;
-            int parent = data.second;
-
-            char col = color[v];
-
-            char newcol = (col == 'Y') ? 'G' : 'Y';
+            char currcol  = (color[v] == 'Y') ? 'G' : 'Y';
 
             for(int ele : graph[v]){
                 if(visit[ele] != 1){
-                    q.offer(new Pair<>(ele,v));
-                    color[ele] = newcol;
+                    color[ele] = currcol;
                     visit[ele] = 1;
-                }else if(color[ele] == color[v]){
-                        return false;
-                    
-                }
-            }
+                    q.offer(ele);
+                }else if(color[ele] == color[v])return false;
+            }       
         }
         return true;
     }
     public boolean isBipartite(int[][] graph) {
         int n = graph.length;
         int[] visit = new int[n];
-        char[] color = new char[n];
+        char color[] = new char[n];
 
         for(int i = 0;i<n;i++){
             if(visit[i] != 1){
-                if(!BFS(i,graph,visit,color)){
+                if(!BFS(i,visit,graph,color)){
                     return false;
                 }
             }
