7755fa26ddfd3ad24115c15f54a7faef8e80515c
Hitaishi <125145810+Hitaishi08@users.noreply.github.com>
2025-07-16 19:22:09 +0530

    Time: 25 ms (6.22%), Space: 47.9 MB (20.44%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -1,64 +1,59 @@
 class Solution {
-    // click expand from 0,store all zeros first and apply BFS
-    static private int[] dir1 = {-1,1,0,0};
-    static private int[] dir2 = {0,0,1,-1};
+    class Pair<V,U>{
+        V first;
+        U second;
 
-    class Pair<U,V>{
-        U first;
-        V second;
-
-        Pair(U first,V second){
+        Pair(V first,U second){
             this.first = first;
             this.second = second;
         }
-
     }
 
-    public void BFS(int mat[][],int[][]visit,Queue<Pair<Pair<Integer,Integer>,Integer>> q,int[][] ans){
+    public static int[] dir1 = {-1,1,0,0};
+    public static int[] dir2 = {0,0,1,-1};
+
+    public void bfs(int[][]visit,int[][] mat,Queue<Pair<Integer,Pair<Integer,Integer>>> q,int[][] ans){
 
         while(!q.isEmpty()){
-            Pair<Pair<Integer, Integer>, Integer> data = q.poll();
+            Pair<Integer,Pair<Integer,Integer>> data = q.poll();
+            Pair<Integer,Integer> curr  = data.second;
 
-            int row = data.first.first;
-            int col = data.first.second;
-            int dist = data.second;
+            int dist = data.first;
+            int row = curr.first;
+            int col = curr.second;
 
             for(int i = 0;i<4;i++){
                 int ii = row + dir1[i];
                 int jj = col + dir2[i];
 
-                if(ii >= 0 && ii < mat.length && jj >= 0 && jj < mat[0].length){
-                    if(mat[ii][jj] == 1 && visit[ii][jj] != 1){
-                        ans[ii][jj] = dist + 1;
-                        visit[ii][jj] = 1;
-                        q.offer(new Pair<>(new Pair<>(ii,jj),dist+1));
-                    }
+                if(ii >= 0 && ii < mat.length && jj >= 0 && jj < mat[0].length && mat[ii][jj] == 1 && visit[ii][jj] != 1){
+                    ans[ii][jj] = dist + 1;
+                    visit[ii][jj] = 1;
+                    q.offer(new Pair<>(dist + 1,new Pair<>(ii,jj)));
                 }
             }
         }
     }
-
     public int[][] updateMatrix(int[][] mat) {
         int n = mat.length;
         int m = mat[0].length;
-        int[][] ans = new int[n][m];
-        int[][] visit = new int[n][m];
 
-        Queue<Pair<Pair<Integer,Integer>,Integer>> q = new LinkedList<>();
+        int[][] visit = new int[n][m];
+        int[][] ans = new int[n][m];
+
+        Queue<Pair<Integer,Pair<Integer,Integer>>> q = new LinkedList<>();
 
         for(int i = 0;i<n;i++){
             for(int j = 0;j<m;j++){
                 if(mat[i][j] == 0){
-                    visit[i][j] = 1;
+                    q.offer(new Pair<>(0,new Pair<>(i,j)));
                     ans[i][j] = 0;
-                    q.offer(new Pair<>(new Pair<>(i,j),0));
                 }
             }
         }
 
-        BFS(mat,visit,q,ans);
+        bfs(visit,mat,q,ans);
 
         return ans;
-        
     }
 }
