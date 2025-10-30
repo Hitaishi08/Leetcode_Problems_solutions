ea07d6565730237f16a726ad4eff8b0acd4793fa
Hitaishi <125145810+Hitaishi08@users.noreply.github.com>
2025-10-30 09:18:02 +0530

    Time: 1 ms (99.89%), Space: 45.7 MB (84.33%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,11 +1,9 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        Arrays.sort(nums);
-        int ele = nums[0];
-        for(int i=1;i<nums.length;i++){
-            ele = ele ^ nums[i];
+        int ele = 0;
+        for(int i = 0;i<nums.length;i++){
+            ele ^= nums[i];
         }
-
         return ele;
     }
 }
