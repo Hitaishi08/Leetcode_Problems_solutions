7a33cd5be34b0246008569f4d6fe57f1c00ec469
Hitaishi <125145810+Hitaishi08@users.noreply.github.com>
2026-01-15 23:51:02 +0530

    Time: 0 ms (100%), Space: 42.8 MB (99.93%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,40 +1,33 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        if(n == 0)return;
-        if(m == 0){
-            for(int i =0;i<nums2.length;i++){
-                nums1[i] = nums2[i];
-            }
-            return;
-        }
-        int[] arr = new int[nums1.length];
+        int i = 0,j=0,p=0;
+        int[] ans = new int[m+n];
 
-        int i = 0,j=0;
-        int k = 0;
         while(i < m && j < n){
             if(nums1[i] <= nums2[j]){
-                arr[k] = nums1[i];
+                ans[p] = nums1[i];
                 i++;
             }else{
-                arr[k] = nums2[j];
+                ans[p] = nums2[j];
                 j++;
             }
-            k++;
+            p++;
         }
 
         while(i < m){
-            arr[k] = nums1[i];
+            ans[p] = nums1[i];
             i++;
-            k++;
+            p++;
         }
 
         while(j < n){
-            arr[k] = nums2[j];
-            k++;
+            ans[p] = nums2[j];
             j++;
+            p++;
         }
-        for(i = 0;i<arr.length;i++){
-            nums1[i] = arr[i];
+
+        for(i = 0;i<n+m;i++){
+            nums1[i] = ans[i];
         }
     }
 }
