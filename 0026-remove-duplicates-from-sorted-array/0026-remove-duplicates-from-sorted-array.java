abe257336d9ae3bb5a05ca26019c60ce4c3a8ae6
Hitaishi <125145810+Hitaishi08@users.noreply.github.com>
2026-05-08 00:03:24 +0530

    Time: 1 ms (76.53%), Space: 46.4 MB (95.13%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -3,14 +3,12 @@ class Solution {
         int i = 0,j = 0;
 
         while(j < nums.length){
-            if(nums[i] == nums[j]){
-                j++;
-            }else{
+            if(nums[i] != nums[j]){
                 nums[i+1] = nums[j];
                 i++;
             }
+            j++;
         }
-
-        return i + 1;
+        return i+1;
     }
 }
