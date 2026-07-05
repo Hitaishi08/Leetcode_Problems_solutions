c4c665bf1ee4f0f946cd14cd1aaf5b7a5ad46bad
Hitaishi <125145810+Hitaishi08@users.noreply.github.com>
2026-07-05 13:54:44 +0530

    Time: 0 ms (100%), Space: 44.9 MB (5.13%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,43 +1,48 @@
 class Solution {
+    public void reverse(int[] nums,int s,int e){
+        while(s < e){
+            int temp = nums[s];
+            nums[s] = nums[e];
+            nums[e] = temp;
+            s++;
+            e--;
+        }
+    }
     public void nextPermutation(int[] nums) {
-        int n = nums.length;
         int pivot = -1;
+        int n = nums.length - 1;
 
-        // find pivot
-        for(int i = n - 2; i >= 0; i--) {
-            if(nums[i] < nums[i + 1]) {
-                pivot = i;
+        for(int i = n;i>0;i--){
+            if(nums[i] > nums[i-1]){
+                pivot = i-1;
                 break;
             }
         }
 
-        // last permutation
-        if(pivot == -1) {
-            reverse(nums, 0, n - 1);
+        if(pivot == -1){
+            reverse(nums,0,n);
             return;
         }
 
-        // find rightmost greater element
-        for(int i = n - 1; i > pivot; i--) {
-            if(nums[i] > nums[pivot]) {
-                int temp = nums[i];
-                nums[i] = nums[pivot];
-                nums[pivot] = temp;
+        // find the first greater element than the pivot element
+        int index = -1;
+
+        for(int i = n;i>=0;i--){
+            if(nums[i] > nums[pivot]){
+                index = i;
                 break;
             }
         }
 
-        // reverse suffix
-        reverse(nums, pivot + 1, n - 1);
-    }
+        // swap the elements
+        int temp = nums[pivot];
+        nums[pivot] = nums[index];
+        nums[index] = temp;
 
-    private void reverse(int[] nums, int i, int j) {
-        while(i < j) {
-            int temp = nums[i];
-            nums[i] = nums[j];
-            nums[j] = temp;
-            i++;
-            j--;
-        }
+        int i = pivot + 1;
+        int j = n;
+
+        reverse(nums,i,j);
+
     }
 }
